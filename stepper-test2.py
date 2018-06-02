48eabe60315e9b9cbf4215c6a0498a18e6d2f329
Carter Allen <bcarter.allen1@gmail.com>
2018-06-02 11:09:47 -0700

    add stepper motor test programs, Adafruit MotorHAT Library, and a QCAnalysis program to determine rotational fidelity of microstepping stepper motor

---
+++ stepper-test2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import Adafruit_MotorHAT, Adafruit_Stepper
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_StepperMotor
+
+import time
+import atexit
+
+mh = Adafruit_MotorHAT(addr = 0x60)
+
+def turnOffMotors():
+        mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+        mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+        mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+        mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+ 
+atexit.register(turnOffMotors)
+
+stepper1 = mh.getStepper(200,1)
+
+stepper1.oneStep(FORWARD, MICROSTEP)
+
+        
